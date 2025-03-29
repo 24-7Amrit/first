43c77be7e3ce8739683a15533d08894957c925a4
24-7Amrit <247amritraj@gmail.com>
2025-03-30 00:25:26 +0530

    29th

--- src/todo/user_dashboard.jsx
+++ src/todo/user_dashboard.jsx
@@ -7,13 +7,19 @@ import { useNavigate } from "react-router-dom";
 export function UserDashboard(){
     
     const [cookies,setCookie,removeCookie]=useCookies('userid');
+    
     let navigate=useNavigate();
-    const [appointments,setAppointments]=useState([{Appointment_id:'',Title:'',Description:'',Date:Date(),UserId:'',}]);
+    
+    const [appointments,setAppointments]=useState([{Appointment_id:0,Title:'',description:'',date:Date(),UserId:''}]);
+    
     function LoadAppointments(){
         axios.get(`http://127.0.0.1:3040/get-appointments/${cookies['UserId']}`)
         .then(response=>{
             setAppointments(response.data);
         })
+        .catch(error => {
+            console.error("Error fetching appointments:", error);
+        });
     }
     useEffect(()=>{
         LoadAppointments();
@@ -24,25 +30,49 @@ export function UserDashboard(){
         navigate('/');
     }
 
+    // return(
+    //     <div> 
+    //         <div>
+
+    //         </div>
+    //         <div>
+    //             <h3>{cookies['userid']} - Dashboard<br/><button onClick={handleSignout}>Signout</button></h3>
+                
+    //             <div>
+    //                 {
+    //                    (appointments.map(appointment=>
+    //                         <div key={appointment.Appointment_id}>
+    //                         {/* <h2>{appointment.Title}</h2>
+    //                         <p>{appointment.description}</p>   */}
+    //                         </div>
+    //                         ))
+    //                     )}
+                        
+                    
+    //             </div>
+    //         </div>
+    //     </div>
+    // )
+
     return(
-        <div> 
-            <div>
+        <div className="row pt-4">
+            <div className="col-7">
 
             </div>
-            <div>
-                <h3>{cookies['userid']} - Dashboard<br/><button onClick={handleSignout}>Signout</button></h3>
-                
-                <div>
+            <div className="col-5">
+                <h3> {cookies['userid']} - Dashboard  <button onClick={handleSignout} className="btn btn-danger">Signout</button> </h3>
+                <div className="mt-4">
                     {
-                        console.log(appointments.map(appointment=>
-                            <div key={appointment.Appointment_id}>
-                                {/* <p>{appointment.Title}</p>
-                                {/* console.log(appointment.Title) */}
-                                {/* <p>{appointment.Description}</p>
-                                <p>{appointment.Date}</p> */}
-                                 */}
-                            </div>)
-                        )
+                        appointments.map(appointment=>
+                            <div key={appointment.Appointment_id} className="alert alert-success alert-dismissible">
+                                <button className="btn btn-close"></button>
+                                <h2 className="alert-title">{appointment.Title}</h2>
+                                <p className="alert-text">{appointment.description}</p>
+                                <p>
+                                    {appointment.date}
+                                </p>
+                            </div>
+                            )
                     }
                 </div>
             </div>
