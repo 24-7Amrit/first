d9c9c44b723ed393cd74f5e2dd4eca385483a651
24-7Amrit <247amritraj@gmail.com>
2025-03-30 23:59:15 +0530

    there was a caase sensitivity problem it got resolved

--- src/todo/user_dashboard.jsx
+++ src/todo/user_dashboard.jsx
@@ -11,12 +11,21 @@ export function UserDashboard(){
     let navigate=useNavigate();
     
     const [appointments,setAppointments]=useState([{Appointment_id:0,Title:'',description:'',date:Date(),UserId:''}]);
-    
+    // there was problem in 17th line case sensitive UserId was written but it should be userid
+    // kindly check this problem coz it is very importnt that it should not get wrong next time
     function LoadAppointments(){
-        axios.get(`http://127.0.0.1:3040/get-appointments/${cookies['UserId']}`)
+        axios.get(`http://127.0.0.1:3040/get-appointments/${cookies['userid']}`)
         .then(response=>{
-            setAppointments(response.data);
+                setAppointments(response.data);
+                console.log("data received");
         })
+        // })
+        // if (response.data && Object.keys(response.data).length > 0) {
+        //     console.log(" Data fetched:", response.data);
+        // } else {
+        //     console.log(" Data is empty!");
+        // }
+        // above lines helped in inding out wether data is comming or not with the help of chatgpt-date30-03-2025
         .catch(error => {
             console.error("Error fetching appointments:", error);
         });
@@ -60,21 +69,20 @@ export function UserDashboard(){
 
             </div>
             <div className="col-5">
-                <h3> {cookies['userid']} - Dashboard  <button onClick={handleSignout} className="btn btn-danger">Signout</button> </h3>
+                <h3> {cookies['Userid']} - Dashboard  <button onClick={handleSignout} className="btn btn-danger">Signout</button> </h3>
                 <div className="mt-4">
                     {
-                        appointments.map(appointment=>
+                               appointments.map(appointment=>
                             <div key={appointment.Appointment_id} className="alert alert-success alert-dismissible">
                                 <button className="btn btn-close"></button>
                                 <h2 className="alert-title">{appointment.Title}</h2>
-                                <p className="alert-text">{appointment.description}</p>
-                                <p>
-                                    {appointment.date}
-                                </p>
+                                <p  className="alert-text">{appointment.description}</p>
+                                <p>{appointment.date}</p>
                             </div>
                             )
                     }
                 </div>
+                
             </div>
         </div>
     )
