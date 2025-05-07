02068b4edc914982b95e5372023ea45257e7ff92
24-7Amrit <247amritraj@gmail.com>
2025-05-07 21:49:17 +0530

    hoistng concept

---
+++ src/components/html/hoisting.js
@@ -0,0 +1,22 @@
+// console.log(myname);
+// var myname="pikoo";
+// show();
+
+// function show(){
+//     console.log('show',age);
+//     var age = 90;
+// } 
+
+
+// random = "ac";
+// console.log(window.random);
+
+
+// function show(){
+//     let abc="js";
+//     let dept=
+// }
+
+
+
+
