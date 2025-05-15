f4d1247ee74f2768b89fac753a859291fdf02970
24-7Amrit <247amritraj@gmail.com>
2025-05-15 23:42:29 +0530

    new

--- src/components/html/hoisting.js
+++ src/components/html/hoisting.js
@@ -1,3 +1,28 @@
+// let promise = new promise((resolve,reject)=>{
+//     console.log("I am a promise");
+//     resolve("done");
+// });
+
+function getData(dataId,getNextData){
+    return new Promise ((res,rej)=>{
+        setTimeout(()=>{
+            console.log("data",dataId);
+            resolvePath("success");
+            if (getNextData){
+                getNextData();
+            }
+        },4000);
+        });
+    }
+// above 10:02:30
+
+
+
+
+
+
+
+
 // console.log(myname);
 // var myname="pikoo";
 // show();
